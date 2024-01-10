4ae103f9671690379fe9a3114e3bf0a534891a96
Olivia1809 <anuduolivia7@gmail.com>
2024-01-10 17:55:51 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:note_app/screen/home_screen.dart';
 
-final colorC = Color.lerp(Colors.green, Colors.purple, 0.5) ?? Colors.green;
-
 final colorScheme = ColorScheme.fromSeed(
   brightness: Brightness.dark,
   seedColor: const Color.fromARGB(210, 125, 121, 153),
-  background: colorC,
+  background: const Color.fromARGB(255, 62, 64, 73),
 );
 
 final theme = ThemeData().copyWith(
   useMaterial3: true,
-  scaffoldBackgroundColor: colorC,
+  scaffoldBackgroundColor: const Color.fromARGB(210, 125, 121, 153),
   colorScheme: colorScheme,
   textTheme: GoogleFonts.ubuntuCondensedTextTheme().copyWith(
     titleSmall: GoogleFonts.ubuntuCondensed(
@@ -37,9 +35,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Notes',
-      theme: theme,
       home: const HomeScreen(),
+      theme: theme,
     );
   }
 }
